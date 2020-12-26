b14b5a23f83abf1b1294fd003c1beadbc02eee2a
mufeili <mufeili1996@gmail.com>
2020-12-26 18:51:34 +0800

    Update

--- examples/gnn.py
+++ examples/gnn.py
@@ -174,7 +174,7 @@ if __name__ == '__main__':
   parser.add_argument(
       '-d',
       '--dataset',
-      choices=['BACE_classification'],
+      choices=['BACE_classification', 'BACE_regression'],
       help='Dataset to use')
   parser.add_argument(
       '-m',
